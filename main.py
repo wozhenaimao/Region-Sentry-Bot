13715b81fa64ca476bdfd0122dfbfa689ef7db2a
Denis <rdmchik@alf-mbpro16.local>
2024-03-31 16:14:22 +0300

    map

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import difflib
 import re
-from datetime import datetime
 
 import discord
 from discord import app_commands
@@ -23,7 +22,7 @@ async def on_ready():
 
 @tree.command(
     name="region",
-    description="Get live region data. :name: name of the region (text value)",
+    description="Get real region data. :name: name of the region (text value)",
 )
 async def first_command(interaction: discord.Interaction, name: str):
 
@@ -36,14 +35,21 @@ async def first_command(interaction: discord.Interaction, name: str):
         return
 
     realName: str = realNameList[0]
-    regionImagePath = FoxholeAPI.hex_to_image(realName)
+    regionImagePath, regionColor, regionType = FoxholeAPI.hex_to_image(realName)
 
     mapData = FoxholeAPI.map(realName).json()
 
+    if regionColor == FoxholeAPI.COLONIALS:
+        color = discord.Colour.from_rgb(122, 156, 116)
+    elif regionColor == FoxholeAPI.WARDENS:
+        color = discord.Colour.from_rgb(12, 69, 171)
+    else:
+        color = discord.Colour.from_rgb(140, 140, 140)
+
     e = discord.Embed(
         title = f'**Region “{re.sub(r"(\w)([A-Z])", r"\1 \2", realName.replace('Hex', ''))}” info**',
-        description = f'Regional data on the __{mapData['dayOfWar']}th__ day of war',
-        color = discord.Colour.from_rgb(61, 78, 58)
+        description = f'Regional data on the __{mapData['dayOfWar']}th__ day of war.\nRegion status: {regionType}',
+        color = color
     )
     e.add_field(
         name='• Total **Enlistments**',
@@ -64,6 +70,36 @@ async def first_command(interaction: discord.Interaction, name: str):
     await interaction.followup.send(file=f, embed=e)
 
     os.remove(f'temp/{regionImagePath}.png')
+
+
+@tree.command(
+    name="map",
+    description="Get real image of the entire map! Be aware, this function will take a while to run",
+)
+async def first_command(interaction: discord.Interaction):
+
+    await interaction.response.defer()
+
+    mapImagePath, totalEnlistments, totalColonialCasualties, totalWardenCasualties, totalCasualties = FoxholeAPI.map_image()
+    warData = FoxholeAPI.war().json()
+    
+    e = discord.Embed(
+        title = f'**Real data of the entire battlefield**',
+        description = f'Conquest started at __{warData['conquestStartTime']}__ (UNIX timestamp)',
+        color = discord.Colour.from_rgb(255, 255, 255)
+    )
+    e.add_field(
+        name='**General information**',
+        value=f'Total enlistments: __{totalEnlistments}__\nTotal casualties: __{totalCasualties}__\nColonial casualties: __{totalColonialCasualties}__\nWarden casualties: __{totalWardenCasualties}__',
+        inline=False
+    )
+    f = discord.File(f'temp/{mapImagePath}.jpg', filename="image.jpg")
+    e.set_image(url='attachment://image.jpg')
+    e.set_footer(text=f'War №{warData['warNumber']} See the image above for complete details of the entire map')
+
+    await interaction.followup.send(file=f, embed=e)
+
+    os.remove(f'temp/{mapImagePath}.jpg')
 
 
 client.run(os.getenv('DISCORD_BOT_TOKEN'))
--- src/foxhole.py
+++ src/foxhole.py
@@ -49,42 +49,47 @@ class FoxholeAPI:
         """
 
         r = FoxholeAPI.war()
-        pprint(str(r.status_code) + ' ::: ' + str(r.headers))
+        pprint('WAR ' + str(r.status_code) + ' ::: ' + str(r.headers))
         pprint(r.json())
 
         r = FoxholeAPI.maps()
-        pprint(str(r.status_code) + ' ::: ' + str(r.headers))
+        pprint('MAPS ' +str(r.status_code) + ' ::: ' + str(r.headers))
         pprint(r.json())
 
         if not name:
             return
         
         r = FoxholeAPI.map(name)
-        pprint(str(r.status_code) + ' ::: ' + str(r.headers))
+        pprint('MAP ' +str(r.status_code) + ' ::: ' + str(r.headers))
         pprint(r.json())
 
         r = FoxholeAPI.map_static(name)
-        pprint(str(r.status_code) + ' ::: ' + str(r.headers))
+        pprint('STATIC ' +str(r.status_code) + ' ::: ' + str(r.headers))
         pprint(r.json())
 
         r = FoxholeAPI.map_dynamic(name)
-        pprint(str(r.status_code) + ' ::: ' + str(r.headers))
+        pprint('DYNAMIC ' +str(r.status_code) + ' ::: ' + str(r.headers))
         pprint(r.json())
 
 
-    def hex_to_image(name: str) -> str:
+    def hex_to_image(name: str, isImage: bool = False):
         """
         Returns unique path id of the image
         """
 
+        mapGeneralData = FoxholeAPI.map(name).json()
         mapStaticData = FoxholeAPI.map_static(name).json()
         mapDynamicData = FoxholeAPI.map_dynamic(name).json()
 
         mapPath = 'static/Images/Maps/Map' + name + '.TGA'
 
-        hex = Image.open(
+        hexImage = Image.open(
             fp=mapPath
         ).convert('RGBA')
+
+        basesCount = 0
+        hasColonialBases = False
+        hasWardenBases = False
 
         for item in mapDynamicData['mapItems']:
 
@@ -97,6 +102,19 @@ class FoxholeAPI:
 
             for key, value in ITEMS.items():
                 if value == item['iconType']:
+                    
+                    if 'base' in key.lower():
+                        if item['teamId'] == FoxholeAPI.COLONIALS:
+                            hasColonialBases = True
+                            basesCount += 1
+                        elif item['teamId'] == FoxholeAPI.WARDENS:
+                            hasWardenBases = True
+                            basesCount -= 1
+                        else:
+                            if basesCount > 0:
+                                basesCount -= 1
+                            elif basesCount < 0:
+                                basesCount += 1
 
                     found = True
                     mapIconPath = 'static/Images/MapIcons/MapIcon' + key + '.TGA'
@@ -116,13 +134,384 @@ class FoxholeAPI:
                             itemImage = Image.merge('RGBA', (R.point(lambda p: p  - 199), G.point(lambda p: p  - 123), B.point(lambda p: p  - 50), A))
 
                     position = (int(position[0] - itemImage.width / 2), int(position[1] - itemImage.height / 2))
-                    hex.alpha_composite(itemImage, position)
+                    hexImage.alpha_composite(itemImage, position)
 
             if not found:
                 raise Exception('[ERROR] could not find image for an item', item)
 
-        pathId = random.randint(10000000, 99999999)
-        hex.save(f'temp/{pathId}.png')
+        color = FoxholeAPI.NONE
+        regionType = 'active frontline'
 
-        return pathId
+        R, G, B, A = hexImage.split()
+        if basesCount > 5:
+            color = FoxholeAPI.COLONIALS
+            if hasWardenBases and isImage:
+                hexImage = Image.merge('RGBA', (R.point(lambda p: p  - 100), G.point(lambda p: p  - 66), B.point(lambda p: p  - 100), A))
+            else:
+                regionType = 'Colonials\' controlled territory'
+                hexImage = Image.merge('RGBA', (R.point(lambda p: p  - 66), G.point(lambda p: p  - 44), B.point(lambda p: p  - 66), A))
+        elif basesCount < 5:
+            color = FoxholeAPI.WARDENS
+            if hasColonialBases and isImage:
+                hexImage = Image.merge('RGBA', (R.point(lambda p: p  - 132), G.point(lambda p: p  - 81), B.point(lambda p: p  - 32), A))
+            else:
+                regionType = 'Wardens\' controlled territory'
+                hexImage = Image.merge('RGBA', (R.point(lambda p: p  - 88), G.point(lambda p: p  - 54), B.point(lambda p: p  - 21), A))
+
+        if basesCount > 5 and hasWardenBases:
+            regionType = 'frontline mostly controlled by Colonials'
+        if basesCount < 5 and hasColonialBases:
+            regionType = 'frontline mostly controlled by Wardens'
+
+        else:
+            ...
+
+        if isImage:
+            return hexImage, mapGeneralData['totalEnlistments'], mapGeneralData['colonialCasualties'], mapGeneralData['wardenCasualties']
+
+        pathId = random.randint(10000000, 99999999)
+        hexImage.save(f'temp/{pathId}.png')
+
+        return pathId, color, regionType
+    
+
+    def map_image():
+        """
+        Warning! This function may change
+        because warapi is very inconsistent.
+        I could not find a more efficient
+        way to create an entire map while 
+        keeping all of the map markers.
+        warapi does not give a sorted list
+        of hex names, so i had to put them 
+        individually
+        """
+
+        hexImages = [
+            [
+                FoxholeAPI.hex_to_image('BasinSionnachHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0,
+                    FoxholeAPI.HEX_SIZE[1] * 0
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('ReachingTrailHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0,
+                    FoxholeAPI.HEX_SIZE[1] * 1,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('CallahansPassageHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0,
+                    FoxholeAPI.HEX_SIZE[1] * 2,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('DeadLandsHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0,
+                    FoxholeAPI.HEX_SIZE[1] * 3,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('UmbralWildwoodHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0,
+                    FoxholeAPI.HEX_SIZE[1] * 4,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('GreatMarchHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0,
+                    FoxholeAPI.HEX_SIZE[1] * 5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('KalokaiHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0,
+                    FoxholeAPI.HEX_SIZE[1] * 6,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('SpeakingWoodsHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -0.75,
+                    FoxholeAPI.HEX_SIZE[1] * 0.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('MooringCountyHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -0.75,
+                    FoxholeAPI.HEX_SIZE[1] * 1.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('LinnMercyHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -0.75,
+                    FoxholeAPI.HEX_SIZE[1] * 2.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('LochMorHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -0.75,
+                    FoxholeAPI.HEX_SIZE[1] * 3.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('HeartlandsHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -0.75,
+                    FoxholeAPI.HEX_SIZE[1] * 4.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('RedRiverHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -0.75,
+                    FoxholeAPI.HEX_SIZE[1] * 5.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('CallumsCapeHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -1.5,
+                    FoxholeAPI.HEX_SIZE[1] * 1,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('StonecradleHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -1.5,
+                    FoxholeAPI.HEX_SIZE[1] * 2,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('KingsCageHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -1.5,
+                    FoxholeAPI.HEX_SIZE[1] * 3,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('SableportHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -1.5,
+                    FoxholeAPI.HEX_SIZE[1] * 4,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('AshFieldsHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -1.5,
+                    FoxholeAPI.HEX_SIZE[1] * 5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('NevishLineHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -2.25,
+                    FoxholeAPI.HEX_SIZE[1] * 1.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('FarranacCoastHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -2.25,
+                    FoxholeAPI.HEX_SIZE[1] * 2.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('WestgateHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -2.25,
+                    FoxholeAPI.HEX_SIZE[1] * 3.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('OriginHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -2.25,
+                    FoxholeAPI.HEX_SIZE[1] * 4.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('OarbreakerHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -3,
+                    FoxholeAPI.HEX_SIZE[1] * 2,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('FishermansRowHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -3,
+                    FoxholeAPI.HEX_SIZE[1] * 3,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('StemaLandingHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * -3,
+                    FoxholeAPI.HEX_SIZE[1] * 4,
+                )
+            ],
+
+            [
+                FoxholeAPI.hex_to_image('HowlCountyHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0.75,
+                    FoxholeAPI.HEX_SIZE[1] * 0.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('ViperPitHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0.75,
+                    FoxholeAPI.HEX_SIZE[1] * 1.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('MarbanHollow', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0.75,
+                    FoxholeAPI.HEX_SIZE[1] * 2.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('DrownedValeHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0.75,
+                    FoxholeAPI.HEX_SIZE[1] * 3.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('ShackledChasmHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0.75,
+                    FoxholeAPI.HEX_SIZE[1] * 4.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('AcrithiaHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 0.75,
+                    FoxholeAPI.HEX_SIZE[1] * 5.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('ClansheadValleyHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 1.5,
+                    FoxholeAPI.HEX_SIZE[1] * 1,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('WeatheredExpanseHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 1.5,
+                    FoxholeAPI.HEX_SIZE[1] * 2,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('ClahstraHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 1.5,
+                    FoxholeAPI.HEX_SIZE[1] * 3,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('AllodsBightHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 1.5,
+                    FoxholeAPI.HEX_SIZE[1] * 4,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('TerminusHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 1.5,
+                    FoxholeAPI.HEX_SIZE[1] * 5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('MorgensCrossingHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 2.25,
+                    FoxholeAPI.HEX_SIZE[1] * 1.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('StlicanShelfHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 2.25,
+                    FoxholeAPI.HEX_SIZE[1] * 2.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('EndlessShoreHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 2.25,
+                    FoxholeAPI.HEX_SIZE[1] * 3.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('ReaversPassHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 2.25,
+                    FoxholeAPI.HEX_SIZE[1] * 4.5,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('GodcroftsHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 3,
+                    FoxholeAPI.HEX_SIZE[1] * 2,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('TempestIslandHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 3,
+                    FoxholeAPI.HEX_SIZE[1] * 3,
+                )
+            ],
+            [
+                FoxholeAPI.hex_to_image('TheFingersHex', True),
+                (
+                    FoxholeAPI.HEX_SIZE[0] * 3,
+                    FoxholeAPI.HEX_SIZE[1] * 4,
+                )
+            ],
+        ]
+
+        totalEnlistments = 0
+        totalColonialCasualties = 0
+        totalWardenCasualties = 0
+        totalCasualties = 0
+
+        mapImage = Image.new('RGBA', (FoxholeAPI.HEX_SIZE[0] * 7, FoxholeAPI.HEX_SIZE[1] * 7))
+        for i, hexImageData in enumerate(hexImages):
+            totalEnlistments += hexImageData[0][1]
+            totalColonialCasualties += hexImageData[0][2]
+            totalWardenCasualties += hexImageData[0][3]
+            totalCasualties += hexImageData[0][2] + hexImageData[0][3]
+            y = int(hexImageData[1][1])
+            position = (int(hexImageData[1][0] + (FoxholeAPI.HEX_SIZE[0] * 7 / 2) - FoxholeAPI.HEX_SIZE[0] * 0.5), y)
+            mapImage.alpha_composite(hexImageData[0][0], position)
+
+        backgroundImage = Image.new("RGB", mapImage.size, (43, 45, 49))
+        backgroundImage.paste(mapImage, mask=mapImage.split()[3])
+
+        pathId = random.randint(10000000, 99999999)
+        backgroundImage.save(f'temp/{pathId}.jpg', 'JPEG', quality=60)
+
+        return pathId, totalEnlistments, totalColonialCasualties, totalWardenCasualties, totalCasualties
     
